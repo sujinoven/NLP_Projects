d04920835737bf174f38a476ff206aff3d8d83d7
sujinoven <sujinoven02@gmail.com>
2026-09-02 12:42:05 +0530

    Remove gitignored files

--- Project 2 Email SPAM vs HAM Classification/src/models/build_model.py
+++
@@ -1,4 +0,0 @@
-from sklearn.naive_bayes import MultinomialNB
-
-def build_model():
-    return MultinomialNB(alpha=1.0)
--- Project 3 News Classification System/src/models/build_model.py
+++
@@ -1,4 +0,0 @@
-from sklearn.svm import LinearSVC
-
-def build_model():
-    return LinearSVC(C=1.0)
